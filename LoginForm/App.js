63aac6321916ffb24c77d0eed7a310adc7d56ed1
rmshimomura <shimomurarodrigo1@gmail.com>
2022-05-26 09:03:35 -0300

    Simple login form

---
+++ LoginForm/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+
+class Inputs extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + '\npassword: ' + pass)
+   }
+   render() {
+      return (
+         <View style = {styles.container}>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#292b2c"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+            
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Password"
+               placeholderTextColor = "#292b2c"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+         </View>
+      )
+   }
+}
+export default Inputs
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 40
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#292b2c',
+      borderWidth: 1,
+      paddingLeft: 10
+   },
+   submitButton: {
+      backgroundColor: '#00ca4e',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   },
+   
+
+})
