ce923b5838773a1f5d4d3f85caafc3cfdca31948
rmshimomura <shimomurarodrigo1@gmail.com>
2022-05-26 08:27:06 -0300

    Using state and props (test)

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,16 +1,64 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PresentationalComponent from './PresentationalComponent'
+import React from "react"
+import { Button, StyleSheet, Text, View } from "react-native"
 
-export default class App extends React.Component {
-   state = {
-      myState: 'This is my state'
+class MyComponent extends React.Component {
+   constructor() {
+      super()
+
+      this.state = {
+         book: 'React Native in Action'
+      }
+   }
+   updateBook() {
+
+      if (this.state.book === 'React Native in Action') {
+         this.setState({
+            book: 'Express in Action'
+         })
+      } else {
+         this.setState({
+            book: 'React Native in Action'
+         })
+      }
    }
    render() {
       return (
-         <View>
-            <PresentationalComponent myState = {this.state.myState}/>
-         </View>
-      );
+         <BookDisplay updateBook={() => this.updateBook()} book={this.state.book} />
+      )
    }
 }
+
+class BookDisplay extends React.Component {
+   render() {
+      return (
+         <View>
+            <Text onPress={this.props.updateBook}>
+               {this.props.book}
+            </Text>
+         </View>
+      )
+   }
+}
+
+export default function App() {
+   return (
+      <View style={styles.container}>
+         <MyComponent />
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#c71585',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+
+   text: {
+      fontSize: 50,
+      color: '#fff',
+      fontFamily: 'Arial',
+   }
+});
